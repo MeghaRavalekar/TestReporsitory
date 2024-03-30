37f413d95ebe92538df6ce3c65e9d2e824156a8e
LENOVO S145 <LENOVO S145@Megha>
2024-03-30 18:59:44 +0530

    This is the first Commit

---
+++ src/BaseClass/FirstBaseClass.java
@@ -0,0 +1,11 @@
+package BaseClass;
+
+public class FirstBaseClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is first base class");
+
+	}
+
+}
---
+++ src/TestCases/Login.java
@@ -0,0 +1,11 @@
+package TestCases;
+
+public class Login {
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is first test");
+
+	}
+
+}
